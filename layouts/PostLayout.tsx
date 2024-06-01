864a6e0142c0795f54479056c16b171cb89f08a0
Ahmet Ömer <ahmetomerv@gmail.com>
2024-06-01 17:38:12 +0200

    update post and other small changes

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -95,7 +95,9 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
               </dd>
             </dl>
             <div className="divide-y divide-gray-200 tracking-tight dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
-              <div className="prose max-w-none pb-8 pt-10 dark:prose-invert">{children}</div>
+              <div className="post-content prose max-w-none pb-8 pt-10 dark:prose-invert">
+                {children}
+              </div>
               <div className="pb-6 pt-6 text-sm text-gray-700 dark:text-gray-300">
                 <Link href={discussUrl(path)} rel="nofollow">
                   Discuss on Twitter
